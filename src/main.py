575a3e2ebf0210c6b50a3c6ce2956cc7c46d26e7
rhowell <richiehowelll@gmail.com>
2024-05-04 11:32:26 -0400

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 from keyboard_lock import KeyboardLock
 
 if __name__ == "__main__":
-    # Create an instance of the KeyboardLock class
     keyboard_lock = KeyboardLock()
-
-    # Start the program logic
     keyboard_lock.start()
