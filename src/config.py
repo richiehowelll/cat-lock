70d9e0bb37a18943936f79446a395cc9a1693303
rhowell <richiehowelll@gmail.com>
2024-05-22 21:49:04 -0400

    create config class

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import json
+
+CONFIG_FILE = "config.json"
+DEFAULT_HOTKEY = "ctrl+shift+l"
+
+
+def load():
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        pass  # Fall back to the default hotkey
+
+
+class Config:
+    def __init__(self):
+        config = load()
+        self.hotkey = config.get("hotkey", DEFAULT_HOTKEY)
+        self.opacity = config.get("opacity", 0.5)
+        self.notifications_enabled = config.get("notificationsEnabled", True)
+
+    def save(self):
+        with open(CONFIG_FILE, "w") as f:
+            config = {
+                "hotkey": self.hotkey,
+                "opacity": self.opacity,
+                "notificationsEnabled": self.notifications_enabled,
+            }
+            json.dump(config, f)
--- src/keyboard_lock.py
+++ src/keyboard_lock.py
@@ -1,17 +1,15 @@
-import json
 import threading
 import time
 import tkinter as tk
 import webbrowser
 from queue import Queue
-import plyer
 
 import keyboard
+import plyer
 from PIL import Image, ImageDraw
 from pystray import Icon, Menu, MenuItem
 
-DEFAULT_HOTKEY = "ctrl+shift+l"
-CONFIG_FILE = "config.json"
+from src.config import Config
 
 
 def open_about():
@@ -27,36 +25,14 @@ class KeyboardLock:
         self.hotkey_thread = None
         self.tray_icon_thread = threading.Thread(target=self.create_tray_icon, daemon=True)
         self.root = None
-        self.hotkey = DEFAULT_HOTKEY
-        self.opacity = .5
-        self.notifications_enabled = True
-        self.load_config()
+        self.config = Config()
         self.show_overlay_queue = Queue()
         self.show_change_hotkey_queue = Queue()
         self.start_hotkey_listener_thread()
         self.tray_icon_thread.start()
 
-    def load_config(self):
-        try:
-            with open(CONFIG_FILE, "r") as f:
-                config = json.load(f)
-                self.hotkey = config.get("hotkey", DEFAULT_HOTKEY)
-                self.opacity = config.get("opacity", 0.5)
-                self.notifications_enabled = config.get("notificationsEnabled", True)
-        except (FileNotFoundError, json.JSONDecodeError):
-            pass  # Fall back to the default hotkey
-
-    def save_config(self):
-        with open(CONFIG_FILE, "w") as f:
-            config = {
-                "hotkey": self.hotkey,
-                "opacity": self.opacity,
-                "notificationsEnabled": self.notifications_enabled,
-            }
-            json.dump(config, f)
-
     def send_lock_notification(self):
-        if self.notifications_enabled:
+        if self.config.notifications_enabled:
             plyer.notification.notify(
                 app_name="CatLock",
                 title="Keyboard Locked",
@@ -67,22 +43,22 @@ class KeyboardLock:
             time.sleep(.1)
 
     def send_notification_in_thread(self):
-        if self.notifications_enabled:
+        if self.config.notifications_enabled:
             notification_thread = threading.Thread(target=self.send_lock_notification, daemon=True)
             notification_thread.start()
             notification_thread.join()
 
     def set_opacity(self, opacity):
-        self.opacity = opacity
-        self.save_config()
+        self.config.opacity = opacity
+        self.config.save()
 
     def set_hotkey(self, new_hotkey):
-        self.hotkey = new_hotkey
-        self.save_config()
+        self.config.hotkey = new_hotkey
+        self.config.save()
 
     def toggle_notifications(self):
-        self.notifications_enabled = not self.notifications_enabled
-        self.save_config()
+        self.config.notifications_enabled = not self.config.notifications_enabled
+        self.config.save()
 
     def change_hotkey(self):
         self.listen_for_hotkey = False
@@ -126,7 +102,7 @@ class KeyboardLock:
                 new_hotkey = hotkey_entry.get()
                 if new_hotkey:
                     self.set_hotkey(new_hotkey)
-                    print(f"Key Unlocked {self.hotkey}")
+                    print(f"Key Unlocked {self.config.hotkey}")
                     print(f"Hotkey changed to: {new_hotkey}")
                     on_closing()
                 else:
@@ -169,8 +145,8 @@ class KeyboardLock:
         self.blocked_keys.clear()
 
         # Manually release the hotkey keys to ensure they're not stuck
-        keyboard.release(self.hotkey)
-        print(f"Keyboard Unlocked {self.hotkey}")
+        keyboard.release(self.config.hotkey)
+        print(f"Keyboard Unlocked {self.config.hotkey}")
         if self.root:
             self.root.destroy()
 
@@ -178,7 +154,7 @@ class KeyboardLock:
         self.root = tk.Tk()
         self.root.attributes('-fullscreen', True)
         self.root.attributes('-topmost', True)
-        self.root.attributes('-alpha', self.opacity)
+        self.root.attributes('-alpha', self.config.opacity)
         self.root.bind('<Button-1>', self.unlock_keyboard)
 
         self.lock_keyboard()
@@ -200,10 +176,10 @@ class KeyboardLock:
             self.hotkey_thread.start()
 
     def hotkey_listener(self):
-        keyboard.add_hotkey(self.hotkey, self.send_hotkey_signal)
+        keyboard.add_hotkey(self.config.hotkey, self.send_hotkey_signal)
         while self.listen_for_hotkey:
             time.sleep(1)
-        keyboard.remove_hotkey(self.hotkey)
+        keyboard.remove_hotkey(self.config.hotkey)
 
     def create_tray_icon(self):
         image = Image.open("../resources/img/icon.png")
@@ -215,7 +191,7 @@ class KeyboardLock:
             MenuItem(
                 "Enable/Disable Notifications",
                 self.toggle_notifications,
-                checked=lambda item: self.notifications_enabled,
+                checked=lambda item: self.config.notifications_enabled,
             ),
             MenuItem("Set Opacity", Menu(
                 MenuItem("5%", lambda: self.set_opacity(0.05)),
