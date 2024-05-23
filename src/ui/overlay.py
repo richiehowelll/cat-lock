e9175df98090589257bd2c10b9a101d477f5e1bd
rhowell <richiehowelll@gmail.com>
2024-05-22 22:40:29 -0400

    create overlay.py

--- src/ui/overlay.py
+++ src/ui/overlay.py
@@ -5,13 +5,12 @@ class Overlay:
     def __init__(self, main):
         self.main = main
 
+    def show(self):
+        self.main.root = tk.Tk()
+        self.main.root.attributes('-fullscreen', True)
+        self.main.root.attributes('-topmost', True)
+        self.main.root.attributes('-alpha', self.main.config.opacity)
+        self.main.root.bind('<Button-1>', self.main.unlock_keyboard)
 
-def show(self):
-    self.root = tk.Tk()
-    self.root.attributes('-fullscreen', True)
-    self.root.attributes('-topmost', True)
-    self.root.attributes('-alpha', self.config.opacity)
-    self.root.bind('<Button-1>', self.unlock_keyboard)
-
-    self.lock_keyboard()
-    self.root.mainloop()
+        self.main.lock_keyboard()
+        self.main.root.mainloop()
