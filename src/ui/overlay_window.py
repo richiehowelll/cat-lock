40baad127c5c2f30c80032bf8fa2bff084146c37
rhowell <richiehowelll@gmail.com>
2024-06-23 22:48:08 -0400

    handle dual monitors

--- src/ui/overlay_window.py
+++ src/ui/overlay_window.py
@@ -1,13 +1,24 @@
 import tkinter as tk
 
+from screeninfo import get_monitors
+
 
 class OverlayWindow:
     def __init__(self, main):
         self.main = main
 
     def open(self) -> None:
+        monitors = get_monitors()
+
+        # Calculate combined geometry of all monitors
+        total_width = sum([monitor.width for monitor in monitors])
+        max_height = max([monitor.height for monitor in monitors])
+        min_x = min([monitor.x for monitor in monitors])
+        min_y = min([monitor.y for monitor in monitors])
+
         self.main.root = tk.Tk()
-        self.main.root.attributes('-fullscreen', True)
+        self.main.root.overrideredirect(True)  # Remove window decorations
+        self.main.root.geometry(f'{total_width}x{max_height}+{min_x}+{min_y}')
         self.main.root.attributes('-topmost', True)
         self.main.root.attributes('-alpha', self.main.config.opacity)
         self.main.root.bind('<Button-1>', self.main.unlock_keyboard)
