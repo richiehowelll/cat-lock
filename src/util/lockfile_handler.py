40e340e987f0d18f3e14f1b284be32a38a32353f
rhowell <richiehowelll@gmail.com>
2024-06-28 11:18:27 -0400

    remove prints

--- src/util/lockfile_handler.py
+++ src/util/lockfile_handler.py
@@ -12,15 +12,13 @@ def check_lockfile():
             pid = int(f.read().strip())
             try:
                 os.kill(pid, signal.SIGTERM)  # Kill the old process
-                print(f"Killed process {pid}")
             except Exception as e:
-                print(f"Process {pid} not found. It might have already been terminated.")
+                # Process not found. It might have already been terminated.
+                pass
     with open(LOCKFILE_PATH, 'w') as f:
         f.write(str(os.getpid()))
-    print(f"Current PID {os.getpid()} written to lockfile")
 
 
 def remove_lockfile():
     if os.path.exists(LOCKFILE_PATH):
         os.remove(LOCKFILE_PATH)
-    print("Lockfile removed")
