d438a532b2a25ec3088d0a1858f361fff27ab708
rhowell <richiehowelll@gmail.com>
2024-06-29 22:13:32 -0400

    more cpu efficient way of handling lock screen

---
+++ src/keyboard_controller/pressed_events_handler.py
@@ -0,0 +1,17 @@
+import time
+
+import keyboard
+
+
+def clear_pressed_events() -> None:
+    while True:
+        # Hotkeys stop working after windows locks & unlocks
+        # https://github.com/boppreh/keyboard/issues/223
+        deleted = []
+        with keyboard._pressed_events_lock:
+            for k in list(keyboard._pressed_events.keys()):
+                item = keyboard._pressed_events[k]
+                if time.time() - item.time > 2:
+                    deleted.append(item.name)
+                    del keyboard._pressed_events[k]
+        time.sleep(1)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-import atexit
-import os
-import subprocess
-import sys
 import threading
 import time
 from queue import Queue
@@ -10,6 +6,7 @@ import keyboard
 
 from src.config.config import Config
 from src.keyboard_controller.hotkey_listener import HotkeyListener
+from src.keyboard_controller.pressed_events_handler import clear_pressed_events
 from src.os_controller.notifications import send_notification_in_thread
 from src.os_controller.tray_icon import TrayIcon
 from src.ui.overlay_window import OverlayWindow
@@ -20,40 +17,18 @@ from src.util.lockfile_handler import check_lockfile, remove_lockfile
 class CatLockCore:
     def __init__(self) -> None:
         self.hotkey_thread = None
-        self.windows_lock_thread = None
-        self.show_overlay_queue = None
-        self.config = None
+        self.clear_pressed_events_thread = threading.Thread(target=clear_pressed_events, daemon=True)
+        self.show_overlay_queue = Queue()
+        self.config = Config()
         self.root = None
-        self.hotkey_lock = None
-        self.listen_for_hotkey = None
-        self.program_running = None
-        self.blocked_keys = None
-        self.changing_hotkey_queue = None
-        self.reset_main_queue = None
-        self.reset()
+        self.hotkey_lock = threading.Lock()
+        self.listen_for_hotkey = True
+        self.program_running = True
+        self.blocked_keys = set()
+        self.changing_hotkey_queue = Queue()
+        self.start_hotkey_listener()
         self.tray_icon_thread = threading.Thread(target=self.create_tray_icon, daemon=True)
         self.tray_icon_thread.start()
-
-    def reset(self) -> None:
-        # hack to deal with stuck hotkeys and keyboard library malfunctioning after windows lock screen
-        try:
-            sys.modules.pop('keyboard')
-        except Exception as e:
-            pass
-        import keyboard
-        self.changing_hotkey_queue = Queue()
-        self.blocked_keys = set()
-        self.program_running = True
-        self.listen_for_hotkey = True
-        self.hotkey_lock = threading.Lock()
-        self.hotkey_thread = None
-        self.root = None
-        self.config = Config()
-        self.show_overlay_queue = Queue()
-        self.start_hotkey_listener()
-        self.windows_lock_thread = threading.Thread(target=self.signal_windows_unlock, daemon=True)
-        self.reset_main_queue = Queue()
-        self.windows_lock_thread.start()
 
     def create_tray_icon(self) -> None:
         TrayIcon(main=self).open()
@@ -75,7 +50,6 @@ class CatLockCore:
         if self.root:
             self.root.destroy()
         keyboard.stash_state()
-        self.reset_main_queue.put(True)
 
     def send_hotkey_signal(self) -> None:
         self.show_overlay_queue.put(True)
@@ -85,23 +59,6 @@ class CatLockCore:
         self.program_running = False
         self.unlock_keyboard()
         icon.stop()
-
-    def signal_windows_unlock(self) -> None:
-        while self.program_running:
-            process_name = 'LogonUI.exe'
-            call_all = 'TASKLIST'
-            no_console_flag = 0x08000000
-            output_all = subprocess.check_output(call_all, creationflags=no_console_flag)
-            output_string_all = str(output_all)
-            was_locked = False
-            while process_name in output_string_all:
-                was_locked = True
-                time.sleep(1)
-                output_all = subprocess.check_output(call_all, creationflags=no_console_flag)
-                output_string_all = str(output_all)
-            if was_locked:
-                self.reset_main_queue.put(True)
-            time.sleep(1)
 
     def start(self) -> None:
         check_lockfile()
@@ -114,8 +71,6 @@ class CatLockCore:
                 overlay = OverlayWindow(main=self)
                 keyboard.stash_state()
                 overlay.open()
-            if not self.reset_main_queue.empty():
-                self.reset()
             time.sleep(.1)
 
 
