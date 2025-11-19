8c5a73258ed2f5d67544581a26c23d0a75dc8ee7
rhowell <richiehowelll@gmail.com>
2025-11-19 15:11:22 -0500

    migrate users to new opacity default

--- src/config/config.py
+++ src/config/config.py
@@ -7,6 +7,8 @@ from src.util.web_browser_util import open_about
 
 BUNDLED_CONFIG_FILE = os.path.join("resources", "config", "config.json")
 DEFAULT_HOTKEY = "ctrl+l"
+DEFAULT_OPACITY = 0.8
+OVERLAY_OPACITY_REVISION = 1  # bump this if we need to change default again
 
 
 def load():
@@ -24,7 +26,7 @@ class Config:
         config = load() or {}
 
         self.hotkey = config.get("hotkey", DEFAULT_HOTKEY)
-        self.opacity = float(config.get("opacity", 0.8))
+        self.opacity = float(config.get("opacity", DEFAULT_OPACITY))
         self.notifications_enabled = config.get("notificationsEnabled", True)
 
         self.overlay_y_percent = int(config.get("overlayYPercent", 25))
@@ -35,8 +37,22 @@ class Config:
 
         self.user_guide_shown = bool(config.get("userGuideShown", False))
 
+        # One-time migration: force all users to the new default opacity
+        self.overlay_opacity_revision = int(config.get("overlayOpacityRevision", 0))
+
+        needs_save = False
+
+        if self.overlay_opacity_revision < OVERLAY_OPACITY_REVISION:
+            self.opacity = DEFAULT_OPACITY
+            self.overlay_opacity_revision = OVERLAY_OPACITY_REVISION
+            needs_save = True
+
+        # If there was no valid config at all, this is a first run.
         if not config:
             open_about()
+            needs_save = True
+
+        if needs_save:
             self.save()
 
     def save(self) -> None:
@@ -48,5 +64,7 @@ class Config:
                 "notificationsEnabled": self.notifications_enabled,
                 "overlayYPercent": self.overlay_y_percent,
                 "userGuideShown": self.user_guide_shown,
+                "overlayOpacityRevision": self.overlay_opacity_revision,
             }
             json.dump(config, f)
+
